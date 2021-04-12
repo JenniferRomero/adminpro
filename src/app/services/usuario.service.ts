517ccfcd7e88a0141f447a2fe553b4746754ce5c
Jennifer <ross_424@hotmail.com>
2021-04-12 11:14:11 -0500

    Implementación perfil

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -7,6 +7,7 @@ import { environment } from 'src/environments/environment';
 
 import { LoginForm } from '../interfaces/login-form-interface';
 import { RegisterForm } from '../interfaces/register-form.interface';
+import { Usuario } from '../models/usuario.model';
 
 const base_url = environment.base_url;
 declare const gapi: any;
@@ -16,6 +17,7 @@ declare const gapi: any;
 })
 export class UsuarioService {
   public auth2: any;
+  public usuario: Usuario;
 
   constructor(
     private http: HttpClient,
@@ -23,6 +25,14 @@ export class UsuarioService {
     private ngZone: NgZone
   ) {
     this.googleInit();
+  }
+
+  get token(): string {
+    return localStorage.getItem('token') || '';
+  }
+
+  get uid(): string {
+    return this.usuario.id || '';
   }
 
   googleInit() {
@@ -48,19 +58,23 @@ export class UsuarioService {
   }
 
   validarToken(): Observable<boolean> {
-    const token = localStorage.getItem('token') || '';
-
     return this.http
       .get(`${base_url}/login/renew`, {
         headers: {
-          'x-token': token,
+          'x-token': this.token,
         },
       })
       .pipe(
-        tap((resp: any) => {
+        map((resp: any) => {
+          console.log(resp);
+
+          const { email, google, nombre, role, id, img = '' } = resp.usuario
+
+          this.usuario = new Usuario(nombre, email, '', img, google, role, id)
+
           localStorage.setItem('token', resp.token);
+          return true;
         }),
-        map((resp) => true),
         catchError((error) => of(false))
       );
   }
@@ -71,6 +85,20 @@ export class UsuarioService {
         localStorage.setItem('token', resp.token);
       })
     );
+  }
+
+  actualizarPerfil(data: { email: string, nombre: string, role: string }) {
+
+    data = {
+      ...data,
+      role : this.usuario.role
+    }
+    return this.http.put(`${base_url}/usuarios/${this.uid}`, data, {
+      headers: {
+        'x-token': this.token,
+      },
+    });
+
   }
 
   login(formData: LoginForm) {
